9c48ab3127d01a0422f7aba4f192e0b349cb1fcb
juniorjmm <josemjunior17@gmail.com>
2021-10-22 16:43:11 -0400

    add suporte para icones no selec

--- src/components/Select.js
+++ src/components/Select.js
@@ -15,7 +15,7 @@ export default function Select({ label, attr, options, value, setter }) {
 
         optionsList.push(
           <option value={opt.value} selected={selected}>
-            {opt.icon ? <i className={opt.icon} /> : ''}
+            {opt.icon ? `${opt.icon} ` : ''}
             {opt.label}
           </option>
         )
